9b2498a576af5e512362c2cfbed7277bf7229cef
ScreaM <gambolanddarvin@mail.ru>
2023-03-04 21:46:46 +0300

    QuestGame ver. 0.0.1

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+// Audio
+const player = document.querySelector('.player'),
+              playBtn = document.querySelector('.play'),
+              audio = document.querySelector('.audio'),
+              imgSrc = document.querySelector('.img__src');
+                              
+// Play function
+function playSong() {
+      player.classList.add('play') 
+      imgSrc.src = '/audio/pause.svg'  
+      audio.play()
+}
+
+// Pause function
+function pauseSong() {
+      player.classList.remove('play')
+      imgSrc.src = "/audio/play.svg"
+      audio.pause()
+}
+
+//Play && Pause btn
+playBtn.addEventListener('click', () => {
+      const isPlaying = player.classList.contains('play')
+      if (isPlaying) {
+            pauseSong()
+      } else {
+            playSong()
+      }
+})
+
+// Preloader
+const audioHide = document.getElementById('none')
+audioHide.style.display = 'none'
+
+window.onload = function () {
+       setTimeout(() => {
+              const preloader = document.getElementById('preloader');
+              preloader.style.display = 'none'
+              audioHide.style.display = 'block'
+              
+       },3500)
+}
+
+// Game // 
+
 let inputUserName = document.createElement('input');
 let buttonSubmit = document.createElement('button');
 let userName;
@@ -9,40 +54,38 @@ inputUserName.placeholder = 'Введите имя';
 buttonSubmit.innerText = 'Ввод';
 
 document.body.append(inputUserName,buttonSubmit)
-console.log(userName)
-
-let ReadyText = document.createElement('h1')
-let ReadyInput = document.createElement('input')
-let ReadyButton = document.createElement('button')
-
-ReadyText.id = "ReadyText"
-ReadyButton.id = "ReadyButton"
-ReadyInput.id = "ReadyInput"
-
-ReadyInput.placeholder = "yes / no ?"
-ReadyButton.innerHTML = "click";
-
 
 buttonSubmit.addEventListener('click', () => {
        userName = inputUserName.value
-       ReadyText.innerHTML = `Привет ${userName}, вы готовы ?`
-       document.body.append(ReadyText,ReadyInput,ReadyButton)
+       readyText.innerHTML = `Привет ${userName}, вы готовы ?`
+       document.body.append(readyText,readyInput,readyButton)
        inputUserName.style.display = "none"
        buttonSubmit.style.display = "none"
 })
 
-ReadyButton.addEventListener("click", () => {
-       if (ReadyInput.value.toLowerCase() === "f") {
-              ReadyText.style.display = "none";
-              ReadyInput.style.display = "none";
-              ReadyButton.style.display = "none";
+let readyText = document.createElement('h1')
+let readyInput = document.createElement('input')
+let readyButton = document.createElement('button')
+
+readyText.id = "ReadyText"
+readyButton.id = "ReadyButton"
+readyInput.id = "ReadyInput"
+
+readyInput.placeholder = "yes / no ?"
+readyButton.innerHTML = "click";
+
+readyButton.addEventListener("click", () => {
+       if (readyInput.value.toLowerCase() === "yes") {
+              readyText.style.display = "none";
+              readyInput.style.display = "none";
+              readyButton.style.display = "none";
               firstQuest.classList.toggle('firstQuest');
               firstQuestText.classList.toggle('firstQuest');
 
-       } else if (ReadyInput.value.toLowerCase() === "no") {
+       } else if (readyInput.value.toLowerCase() === "no") {
               location.reload()
        } else {
-              console.log("Error 666")
+              console.log("%cError 666" , "color:red;text-transform:uppercase;font-size:35px;")
        }
 })
 
@@ -63,41 +106,41 @@ trueButton.addEventListener("click", () => {
        alert('Complete (Hell =)) )') 
        firstQuestText.style.display = "none"
        firstQuest.style.display = "none"
-       document.body.append(QuestTwoButton,QuestTwoInput,QuestTwoText,QuestTwoButtonHelp)
+       document.body.append(questTwoButton,questTwoInput,questTwoText,questTwoButtonHelp)
 })
 
 
-let QuestTwoText = document.createElement('h1');
-let QuestTwoInput = document.createElement('input');
-let QuestTwoButton = document.createElement('button');
-let QuestTwoButtonHelp = document.createElement('button');
+let questTwoText = document.createElement('h1');
+let questTwoInput = document.createElement('input');
+let questTwoButton = document.createElement('button');
+let questTwoButtonHelp = document.createElement('button');
 
-QuestTwoText.id = "QuestTwoText"
-QuestTwoInput.id = "QuestTwoInput"
-QuestTwoButton.id = "QuestTwoButton"
-QuestTwoButtonHelp.id = "QuestTwoButtonHelp"
+questTwoText.id = "QuestTwoText"
+questTwoInput.id = "QuestTwoInput"
+questTwoButton.id = "QuestTwoButton"
+questTwoButtonHelp.id = "QuestTwoButtonHelp"
 
-QuestTwoInput.placeholder = "Ответ"
-QuestTwoButton.innerHTML = "answer";
-QuestTwoButtonHelp.innerHTML = "Подсказка по кнопке"
+questTwoInput.placeholder = "Ответ"
+questTwoButton.innerHTML = "answer";
+questTwoButtonHelp.innerHTML = "Подсказка по кнопке"
 
 trueButton.addEventListener("click", () => {
-       QuestTwoText.innerHTML = "Решите математическую задачу <br> x³ + y³ + z³ = k. K — каждое из чисел от 1 до 100. Найти x, y и z. <br>(из числа 42). P.S Что бы ответить надо найти еще кнопку =) "
-       document.body.append(QuestTwoButton,QuestTwoInput,QuestTwoText,QuestTwoButtonHelp)
-       QuestTwoText.style.width = "700px"
+       questTwoText.innerHTML = "Решите математическую задачу <br> x³ + y³ + z³ = k. K — каждое из чисел от 1 до 100. Найти x, y и z. <br>(из числа 42). P.S Что бы ответить надо найти еще кнопку =) "
+       document.body.append(questTwoButton,questTwoInput,questTwoText,questTwoButtonHelp)
+       questTwoText.style.width = "700px"
 })
 
-QuestTwoButtonHelp.addEventListener('click', () => {
-       QuestTwoButtonHelp.innerHTML = "Кнопка где-то в верху"
+questTwoButtonHelp.addEventListener('click', () => {
+       questTwoButtonHelp.innerHTML = "Кнопка где-то в верху"
 })
 
-QuestTwoButton.addEventListener('click', () => {
-       if (QuestTwoInput.value === "42") {
+questTwoButton.addEventListener('click', () => {
+       if (questTwoInput.value === "42") {
               alert("Правильно,а решение выглядит так (-80538738812075974)³ + 80435758145817515³ + 12602123297335631³ = 42.")
-              QuestTwoText.style.display = "none"
-              QuestTwoButton.style.display = "none"
-              QuestTwoInput.style.display = "none"
-              QuestTwoButtonHelp.style.display = "none"
+              questTwoText.style.display = "none"
+              questTwoButton.style.display = "none"
+              questTwoInput.style.display = "none"
+              questTwoButtonHelp.style.display = "none"
               document.body.append(halfway,moveOnFour)
        } else {
               console.log("Error")
@@ -111,102 +154,102 @@ moveOnFour.innerHTML = "move on";
 halfway.id = "halfway"
 moveOnFour.id = "moveOnFour"
 
-QuestTwoButton.addEventListener('click', () => {
+questTwoButton.addEventListener('click', () => {
        halfway.innerHTML = `И так ${userName} ты уже прошел пол пути. Дальше больше!!!(Нет)`
 })
 
 moveOnFour.addEventListener('click', () => {
-       QuestTwoButton.style.display = "none"
+       questTwoButton.style.display = "none"
        moveOnFour.style.display = "none"
        halfway.style.display = "none"
-       document.body.append(MathText,MathButton)
+       document.body.append(mathText,mathButton)
 })
 
-const MathText = document.createElement("h1")
-const MathButton = document.createElement("button")
-MathText.id = "MathText"
-MathButton.id = "MathButton"
-MathButton.textContent = "GOOOO!!!"
+const mathText = document.createElement("h1")
+const mathButton = document.createElement("button")
+mathText.id = "MathText"
+mathButton.id = "MathButton"
+mathButton.textContent = "GOOOO!!!"
 
 moveOnFour.addEventListener('click',() => {
-       MathText.innerHTML = "Щас будет несколько математический задач =)"
+       mathText.innerHTML = "Щас будет несколько математический задач =)"
 })
 
-MathButton.addEventListener('click',() => {
-       MathText.style.display = "none"
-       MathButton.style.display = "none"
-       document.body.append(MathTestOneText,MathTestOneInput,MathTestOneButton)
+mathButton.addEventListener('click',() => {
+       mathText.style.display = "none"
+       mathButton.style.display = "none"
+       document.body.append(mathTestOneText,mathTestOneInput,mathTestOneButton)
 })
 
-const MathTestOneText = document.createElement('h1')
-const MathTestOneInput = document.createElement('input')
-const MathTestOneButton = document.createElement('button')
+const mathTestOneText = document.createElement('h1')
+const mathTestOneInput = document.createElement('input')
+const mathTestOneButton = document.createElement('button')
 
-MathTestOneText.id = "MathTestOneText"
-MathTestOneInput.id = "MathTestOneInput"
-MathTestOneButton.id = "MathTestOneButton"
-MathTestOneButton.textContent = "Answer"
+mathTestOneText.id = "MathTestOneText"
+mathTestOneInput.id = "MathTestOneInput"
+mathTestOneButton.id = "MathTestOneButton"
+mathTestOneButton.textContent = "Answer"
 
-MathButton.addEventListener('click',() => {
+mathButton.addEventListener('click',() => {
        MathTestOneText.innerHTML = "Какое число получится, если перемножить все цифры на цифровой клавиатуре телефона?"
 })
 
-MathTestOneButton.addEventListener('click', () => {
-       if (MathTestOneInput.value.toLowerCase() === "0") {
+mathTestOneButton.addEventListener('click', () => {
+       if (mathTestOneInput.value.toLowerCase() === "0") {
               alert("Правильно!!")
-              MathTestOneText.style.display = "none"
-              MathTestOneInput.style.display = "none"
-              MathTestOneButton.style.display = "none"
-              document.body.append(MathTestTwoText,MathTestTwoInput,MathTestTwoButton)
+              mathTestOneText.style.display = "none"
+              mathTestOneInput.style.display = "none"
+              mathTestOneButton.style.display = "none"
+              document.body.append(mathTestTwoText,mathTestTwoInput,mathTestTwoButton)
        } else {
               alert("Неправильно")
        }
 })
 
-const MathTestTwoText = document.createElement('h1')
-const MathTestTwoInput = document.createElement('input')
-const MathTestTwoButton = document.createElement('button')
+const mathTestTwoText = document.createElement('h1')
+const mathTestTwoInput = document.createElement('input')
+const mathTestTwoButton = document.createElement('button')
 
-MathTestTwoText.id = "MathTestTwoText"
-MathTestTwoInput.id = "MathTestTwoInput"
-MathTestTwoButton.id = "MathTestTwoButton"
-MathTestTwoButton.textContent = "Answer"
+mathTestTwoText.id = "MathTestTwoText"
+mathTestTwoInput.id = "MathTestTwoInput"
+mathTestTwoButton.id = "MathTestTwoButton"
+mathTestTwoButton.textContent = "Answer"
 
-MathTestOneButton.addEventListener('click', () => {
+mathTestOneButton.addEventListener('click', () => {
        MathTestTwoText.innerHTML = "Где можно прибавить 2 к 11 и получить 1?"
 })
 
-MathTestTwoButton.addEventListener('click', () => {
-       if (MathTestTwoInput.value.toLowerCase() === "на часах") {
+mathTestTwoButton.addEventListener('click', () => {
+       if (mathTestTwoInput.value.toLowerCase() === "на часах") {
               alert('Это тоже верно!')
-              MathTestTwoText.style.display = "none"
-              MathTestTwoInput.style.display = "none"
-              MathTestTwoButton.style.display = "none"
-              document.body.append(MathTestThreeText,MathTestThreeInput,MathTestThreeButton)
+              mathTestTwoText.style.display = "none"
+              mathTestTwoInput.style.display = "none"
+              mathTestTwoButton.style.display = "none"
+              document.body.append(mathTestThreeText,mathTestThreeInput,mathTestThreeButton)
        } else {
               alert('Не правильно')
        }
 })
 
-const MathTestThreeText = document.createElement('h1')
-const MathTestThreeInput = document.createElement('input')
-const MathTestThreeButton = document.createElement('button')
+const mathTestThreeText = document.createElement('h1')
+const mathTestThreeInput = document.createElement('input')
+const mathTestThreeButton = document.createElement('button')
 
-MathTestThreeText.id = "MathTestThreeText"
-MathTestThreeInput.id = "MathTestThreeInput"
-MathTestThreeButton.id = "MathTestThreeButton"
-MathTestThreeButton.textContent = "Answer"
+mathTestThreeText.id = "MathTestThreeText"
+mathTestThreeInput.id = "MathTestThreeInput"
+mathTestThreeButton.id = "MathTestThreeButton"
+mathTestThreeButton.textContent = "Answer"
 
-MathTestTwoButton.addEventListener('click', () => {
-       MathTestThreeText.innerHTML = "Утка получила 9 долларов, паук — 36 долларов, пчела — 27 долларов. Основываясь на этой информации, сколько денег дадут кошке?"
+mathTestTwoButton.addEventListener('click', () => {
+       mathTestThreeText.innerHTML = "Утка получила 9 долларов, паук — 36 долларов, пчела — 27 долларов. Основываясь на этой информации, сколько денег дадут кошке?"
 })
 
-MathTestThreeButton.addEventListener('click', () => {
-       if (MathTestThreeInput.value.toLowerCase() === "18") {
+mathTestThreeButton.addEventListener('click', () => {
+       if (mathTestThreeInput.value.toLowerCase() === "18") {
               alert('Я думал ты не настолько умный')
-              MathTestThreeText.style.display = "none"
-              MathTestThreeInput.style.display = "none"
-              MathTestThreeButton.style.display = "none"
+              mathTestThreeText.style.display = "none"
+              mathTestThreeInput.style.display = "none"
+              mathTestThreeButton.style.display = "none"
               document.body.append(finalMathTest,finalMathTestBtn)
        } else {
               alert('Не верно')
@@ -221,55 +264,55 @@ finalMathTestBtn.id = "finalMathTestBtn"
 
 finalMathTestBtn.textContent = "Go =*("
 
-MathTestThreeButton.addEventListener('click', () => {
+mathTestThreeButton.addEventListener('click', () => {
        finalMathTest.innerHTML = "Остался последний вопрос"
 })
 
 finalMathTestBtn.addEventListener('click', () => {
        finalMathTest.style.display = "none"
        finalMathTestBtn.style.display = "none"
-       document.body.append(MathTestFourText,MathTestFourInput,MathTestFourButton)
+       document.body.append(mathTestFourText,mathTestFourInput,mathTestFourButton)
 })
 
 
-const MathTestFourText = document.createElement('h1')
-const MathTestFourInput = document.createElement('input')
-const MathTestFourButton = document.createElement('button')
-MathTestFourText.id = "MathTestFourText"
-MathTestFourInput.id = "MathTestFourInput"
-MathTestFourButton.id = "MathTestFourButton"
+const mathTestFourText = document.createElement('h1')
+const mathTestFourInput = document.createElement('input')
+const mathTestFourButton = document.createElement('button')
+mathTestFourText.id = "MathTestFourText"
+mathTestFourInput.id = "MathTestFourInput"
+mathTestFourButton.id = "MathTestFourButton"
 
 
-MathTestFourButton.textContent = "Answer"
+mathTestFourButton.textContent = "Answer"
 
 finalMathTestBtn.addEventListener('click', () => {
-       MathTestFourText.innerHTML = "Сколько сторон у круга? {Здесь без стиля т.к это изюминка это го вопроса =) }"
+       mathTestFourText.innerHTML = "Сколько сторон у круга? {Здесь без стиля т.к это изюминка это го вопроса =) }"
 })
 
-MathTestFourButton.addEventListener('click', () => {
-       if (MathTestFourInput.value.toLowerCase() === "2") {
+mathTestFourButton.addEventListener('click', () => {
+       if (mathTestFourInput.value.toLowerCase() === "2") {
               alert('Внутри и снаружи =)')
-              MathTestFourButton.style.display = 'none'
-              MathTestFourInput.style.display = 'none'
-              MathTestFourText.style.display = 'none'
-              document.body.append(FinalText,ResetBtn)
+              mathTestFourButton.style.display = 'none'
+              mathTestFourInput.style.display = 'none'
+              mathTestFourText.style.display = 'none'
+              document.body.append(finalText,resetBtn)
        }else {
               alert('Подумай еще')
        }
 })
 
-const FinalText = document.createElement('h1')
-const ResetBtn = document.createElement('button')
-FinalText.id = "FinalText"
-ResetBtn.id = "ResetBtn"
+const finalText = document.createElement('h1')
+const resetBtn = document.createElement('button')
+finalText.id = "FinalText"
+resetBtn.id = "ResetBtn"
 
-ResetBtn.textContent = "RESET"
+resetBtn.textContent = "RESET"
 
-MathTestFourButton.addEventListener('click', () => {
-       FinalText.innerHTML = `Поздравляем ${userName} это ФИНАЛ!!!( Т.к у автора закончились идеи =)) ) ,но вы можете ресетнуть нажав по кнопке RESET`
+mathTestFourButton.addEventListener('click', () => {
+       finalText.innerHTML = `Поздравляем ${userName} это ФИНАЛ!!!( Т.к у автора закончились идеи =)) ) ,но вы можете ресетнуть нажав по кнопке RESET`
 })
 
-ResetBtn.addEventListener('click', () => {
+resetBtn.addEventListener('click', () => {
        location.reload()
 })
 
